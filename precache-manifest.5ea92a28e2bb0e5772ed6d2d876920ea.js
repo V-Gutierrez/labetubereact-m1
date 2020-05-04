b39be7c95e59b00e7886f6109acb6712ee79f3b2
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2020-05-03 21:55:54 -0400

    Updates

---
+++ precache-manifest.5ea92a28e2bb0e5772ed6d2d876920ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d568c779da99bac704fa021dded620b0",
+    "url": "/labetubereact-m1/index.html"
+  },
+  {
+    "revision": "a29b7b92138f01643106",
+    "url": "/labetubereact-m1/static/css/main.03197273.chunk.css"
+  },
+  {
+    "revision": "636e3b1a9f00c5dfda95",
+    "url": "/labetubereact-m1/static/js/2.6ad5bf4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/labetubereact-m1/static/js/2.6ad5bf4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29b7b92138f01643106",
+    "url": "/labetubereact-m1/static/js/main.61fdb1b8.chunk.js"
+  },
+  {
+    "revision": "f4b1570f7b1eeb3202b4",
+    "url": "/labetubereact-m1/static/js/runtime-main.528bb859.js"
+  },
+  {
+    "revision": "998d7c49d2e63469f670b7dcdd7924e1",
+    "url": "/labetubereact-m1/static/media/labetube.998d7c49.png"
+  }
+]);
